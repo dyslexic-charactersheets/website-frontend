fbdc3489cf4d6c939acc6584f317872250175dee
Marcus Downing <marcus@bang-on.net>
2019-08-12 00:46:45 +0100

    Safe preview

--- app/server.js
+++ app/server.js
@@ -14,7 +14,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 // app.use(bodyParser());
 
 // and the app view engine with handlebars
